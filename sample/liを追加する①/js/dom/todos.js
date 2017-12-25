ad61d8d4e02feeacc1299093490f3bb30f927020
kiryu kazuma <nakanishiyoshiaki0915@gmail.com>
2017-12-25 21:58:07 +0900

    Add files via upload

---
+++ sample/liを追加する①/js/dom/todos.js
@@ -0,0 +1,59 @@
+var todo = todo || {};
+todo.dom = todo.dom || {};
+todo.dom.todos = todo.dom.todos || {};
+
+(function(_) {
+	_.add = function(todo) {
+		// <li> 要素を作る
+		var li = document.createElement('LI');
+		li.id = 'todo-' + todo.id;
+		li.classList.add('todo');
+		if (todo.done) { // 完了した Todo 項目の色変更に必要
+			li.classList.add('todo-done');
+		}
+
+		var input = document.createElement('INPUT');
+		input.id = 'todo-' + todo.id + '-checkbox';
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		var label = document.createElement('LABEL');
+		label.htmlFor = input.id;
+		label.textContent = todo.title;
+
+		var div = document.createElement('DIV');
+		div.classList.add('todo-operation');
+		var editButton = document.createElement('BUTTON');
+		editButton.value = 'edit';
+		editButton.classList.add('todo-operation-edit');
+		editButton.textContent = '✎';
+		var deleteButton = document.createElement('BUTTON');
+		deleteButton.value = 'delete';
+		deleteButton.classList.add('todo-operation-delete');
+		deleteButton.textContent = '×';
+
+		// ここからはappendChildで要素の子要素に要素を追加する
+		/*
+		DOMツリー構造
+		<ul>
+			<li>
+				<input>
+				<label>
+				<div>
+					<button>
+					<button>
+		*/
+		
+		// div => button
+		div.appendChild(editButton);
+		div.appendChild(deleteButton);
+
+		// li => input/label/div
+		li.appendChild(input);
+		li.appendChild(label);
+		li.appendChild(div);
+
+		// <ul class="todos">要素の子要素に追加
+		var todos = document.querySelector('.todos'); // .todosの代わりにULでもOK!!
+		todos.appendChild(li);
+	};
+})(todo.dom.todos);
